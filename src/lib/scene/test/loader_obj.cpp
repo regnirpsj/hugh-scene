46b31ab0a6ef54347b4a6aaccaf88453cf846738
jan p springer <jsd@igroup.org>
2016-11-28 09:58:24 +0000

    added: support for .obj file testing from installation path

--- src/lib/scene/test/loader_obj.cpp
+++ src/lib/scene/test/loader_obj.cpp
@@ -18,6 +18,11 @@
 
 // includes, project
 
+#include <hugh/scene/node/group.hpp>
+#include <hugh/scene/visitor/print.hpp>
+#include <hugh/support/chrono.hpp>
+#include <hugh/support/chrono_io.hpp>
+#include <hugh/support/type_info.hpp>
 #include <loader/obj.hpp>
 
 #define HUGH_USE_TRACE
@@ -29,6 +34,28 @@
 namespace {
   
   // types, internal (class, enum, struct, union, typedef)
+
+  class simple_print : public hugh::scene::visitor::print {
+
+  public:
+
+    using order = hugh::scene::visitor::print::order;
+    
+    explicit simple_print(std::ostream& a, order b = order::dfs)
+      : hugh::scene::visitor::print(a, b)
+    {}
+
+    virtual void visit(hugh::scene::node::base& a)
+    {
+      boost::io::ios_all_saver const ias(os_);
+
+      os_ << std::string((2 * indent_), ' ')
+          << hugh::support::demangle(typeid(a)) << ':'
+          << a.name.get()
+          << crlf_;
+    }
+    
+  };
 
   // variables, internal
 
@@ -84,6 +111,7 @@ namespace {
 
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
+#include <boost/test/output_test_stream.hpp>
 
 BOOST_AUTO_TEST_CASE(test_hugh_scene_loader_obj_cube)
 {
@@ -105,4 +133,61 @@ BOOST_AUTO_TEST_CASE(test_hugh_scene_loader_obj_squares)
   }
   
   BOOST_CHECK(nullptr != hugh::scene::file::obj::load(str));
+}
+
+BOOST_AUTO_TEST_CASE(test_hugh_scene_loader_obj_file)
+{
+  static std::array<std::string const, 19> const file_names = {
+    {
+      "/buddha/buddha.obj",
+      "/conference/conference.obj",
+      "/cornell-box/CornellBox-Empty-CO.obj",
+      "/cornell-box/CornellBox-Empty-RG.obj",
+      "/cornell-box/CornellBox-Empty-Squashed.obj",
+      "/cornell-box/CornellBox-Empty-White.obj",
+      "/cornell-box/CornellBox-Glossy.obj",
+      "/cornell-box/CornellBox-Mirror.obj",
+      "/cornell-box/CornellBox-Original.obj",
+      "/cornell-box/CornellBox-Sphere.obj",
+      "/cornell-box/CornellBox-Water.obj",
+      "/cube/cube.obj",
+      "/dabrovic-sponza/sponza.obj",
+      "/dragon/dragon.obj",
+      "/lost-empire/lost_empire.obj",
+      "/mitsuba/mitsuba.obj",
+      "/sibenik/sibenik.obj",
+      "/sportscar/sportsCar.obj",
+      "/teapot/teapot.obj",
+    }
+  };
+  static std::string const prefix(std::string(CMAKE_INSTALL_PREFIX) + "/share/hugh/data/mcguire");
+  
+  using namespace hugh::scene;
+  using namespace hugh::support;
+  
+  timer t;
+  
+  for (auto f : file_names) {
+    auto const fname(prefix + f);
+
+    t.reset();
+
+    std::unique_ptr<node::group> g(file::obj::load(fname));
+
+    BOOST_TEST_MESSAGE("load time was " << pretty_print(t.lapse()) << " for '" << fname << "'");
+    BOOST_CHECK       (nullptr != g);
+
+#if 0
+    if (nullptr != g) {
+      boost::test_tools::output_test_stream output;
+
+      simple_print sp(output, simple_print::order::dfs);
+      
+      g->accept(sp);
+        
+      BOOST_CHECK       (!output.is_empty(false));
+      BOOST_TEST_MESSAGE(output.str());
+    }
+#endif
+  }
 }
